49bee12880d8814a6ee587702a8e8f8337f582d6
pauxiel <paul.scholes50@yahoo.com>
2021-01-09 17:09:30 -0800

    anooejek

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
     // src/App.js
 
-import React, { Component } from 'react';
-import Profiles from './components/profiles' 
-import * as ProfileApi from './ProfileApi'
+// import React, { Component } from 'react';
+import Profiles from './components/Profiles' 
+// import * as ProfileApi from './ProfileApi'
+
+// class App extends Component {
+      
+//      state = {
+//       profiles :  []
+//      }
+  
+//      componentDidMount() {
+//       ProfileApi.getAll()
+//       .then((profiles) => {
+//            this.setState(() => ({
+//               profiles
+//            }))
+//       })
+//      }
+  
+//       render () {
+//         return (
+//           // JSX to render goes here...
+//           <Profiles profiles = {this.state.profiles}/>
+//         );
+//       }
+//     }
+
+//     export default App;
+
+
+
+    import React, {Component} from 'react';
+// import Contacts from './components/contacts';
 
 class App extends Component {
-      
-     state = {
-      profiles :  []
-     }
-  
-     componentDidMount() {
-      ProfileApi.getAll()
-      .then((profiles) => {
-           this.setState(() => ({
-              profiles
-           }))
-      })
-     }
-  
-      render () {
+    render() {
         return (
-          // JSX to render goes here...
-          <Profiles profiles = {this.state.profiles}/>
-        );
-      }
+            <Profiles profiles={this.state.profiles} />
+        )
     }
 
-    export default App;
+    state = {
+        profiles: []
+    };
+
+    componentDidMount() {
+        fetch('http://api.enye.tech/v1/challenge/records')
+            .then(res => res.json())
+          .then((data) => {
+                // var newData = data.concat([data])
+                this.setState({ profiles: data })
+            })
+            .catch(console.log)
+    }
+}
+
+export default App;
